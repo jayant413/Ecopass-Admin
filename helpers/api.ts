41d7266f0a8e73fd63058b77a24fa2a1a789e61d
jayant413 <jayantsawarkar4103@gmail.com>
2024-04-24 15:22:04 +0530

    fix: production backend link

--- helpers/api.ts
+++ helpers/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "https://be-project-backend.vercel.app/api/v1",
-//   withCredentials: true,
-// });
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://be-project-backend.vercel.app/api/v1",
   withCredentials: true,
 });
+// const api = axios.create({
+//   baseURL: "http://localhost:8000/api/v1",
+//   withCredentials: true,
+// });
 
 export default api;
